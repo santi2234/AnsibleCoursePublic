dede730b07b81dce1bb81985bd2307b8ef33d3a4
santi2234 <santio@hotmail.be>
2023-11-13 22:50:04 +0100

    +

--- scripts/ansibleMachine.sh
+++ scripts/ansibleMachine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo add-apt-repository --yes --update ppa:ansible/ansible # get latest repository from ansible
-sudo apt install ansible # install ansible
-sudo touch .ansible.conf /~;
+sudo apt install --yes ansible # install ansible
 sudo cp /vagrant/files/ansible.cfg /etc/ansible/ansible.cfg;
 sudo cp /vagrant/inventory/hosts /etc/ansible/hosts;
+sudo ssh-keygen -b 2048 -t rsa -f /tmp/sshkeyAnsibleController -q -N "";
 
